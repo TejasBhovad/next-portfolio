e24f57c61b335c114df8c2eb10e939eeb6d1ca16
Tejas Bhovad <tejasbhovad@gmail.com>
2023-06-25 17:25:00 +0530

    fixed issues in Safari 

--- components/logos/Arrow.jsx
+++ components/logos/Arrow.jsx
@@ -1,11 +1,6 @@
 import * as React from "react";
 const Arrow = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 32 40"
-    {...props}
-    transform="rotate(270)"
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 40" {...props}>
     <title>{"Scroll to Bottom"}</title>
     <path
       d="m14 16 5.79-7.4a1 1 0 1 0-1.57-1.23l-5.79 7.4a2 2 0 0 0 0 2.43l5.79 7.4a1 1 0 1 0 1.57-1.23Z"
